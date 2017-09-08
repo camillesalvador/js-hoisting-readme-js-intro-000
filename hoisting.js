a10b8d200119267184c3ebd5dd04de47c4896208
Camille Salvador <camille.salvador@outlook.com>
2017-09-08 18:02:33 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -17,12 +17,14 @@ function crazy() {
 
 function sayMyName() {
   // fix the code in here:
-  var name = "Cricky";
+  //var name = "Cricky";
 
-  sayMy();
+  
 
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
+    
   }
+  sayMy();
 }
